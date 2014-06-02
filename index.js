da0945a5da5f0b31fd87b0535dde3dc33acdc5e4
Jan Ulrich <julrich@salesforce.com>
2014-06-02 14:38:23 -0700

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var through = require('through');
+var through2 = require('through2');
 var os = require('os');
 var path = require('path');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var File = gutil.File;
 var Buffer = require('buffer').Buffer;
+
+//module.exports = function(filename, opt){
+//
+//
+//  return through2({objectMode: true}, function(chunk, enc, callback){
+//    console.log(chunk);
+//    //if (data.isString()) this.push(chunk);
+//    //callback();
+//  });
+//}
 
 module.exports = function(fileName, opt){
   if (!fileName) throw new PluginError('gulp-concat', 'Missing fileName option for gulp-concat');
@@ -16,14 +26,15 @@ module.exports = function(fileName, opt){
   var firstFile = null;
   var newLineBuffer = opt.newLine ? new Buffer(opt.newLine) : null;
 
-  function bufferContents(file){
-    if (file.isNull()) return; // ignore
-    if (file.isStream()) return this.emit('error', new PluginError('gulp-concat',  'Streaming not supported'));
+  function bufferContents(data, encoding, callback){
+    if (data == null) return; // ignore
+    //if (data.isStream()) return this.emit('error', new PluginError('gulp-concat',  'Streaming not supported'));
 
     if (firstFile && newLineBuffer) buffer.push(newLineBuffer);
-    if (!firstFile) firstFile = file;
+    if (!firstFile) firstFile = data;
 
-    buffer.push(file.contents);
+    this.push(data.contents);
+    callback();
   }
 
   function endStream(){
@@ -44,5 +55,5 @@ module.exports = function(fileName, opt){
     this.emit('end');
   }
 
-  return through(bufferContents, endStream);
+  return through2({objectMode: true}, bufferContents, endStream);
 };
